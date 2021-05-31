d79ac90a366bf845844783a31e7c1933c0bf6992
Outlande <16231215@buaa.edu.cn>
2021-05-31 20:32:15 +0800

    realsense

--- ElasticFusion/build.sh
+++ ElasticFusion/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# #!/bin/bash
 
 # mkdir deps &> /dev/null
 # cd deps
@@ -24,12 +24,20 @@
 #     sudo add-apt-repository ppa:openjdk-r/ppa
 #     sudo apt-get update
 #     sudo apt-get install cuda-8-0
+# elif [[ $version == *"18.04"* ]] ; then
+#     wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+#     sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+#     rm cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+#     sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+#     sudo add-apt-repository ppa:openjdk-r/ppa
+#     sudo apt-get update
+#     sudo apt-get install cuda-10-0
 # else
-#     echo "Don't use this on anything except 14.04, 15.04, or 16.04"
+#     echo "Don't use this on anything except 14.04, 15.04, 16.04 or 18.04"
 #     exit
 # fi
 
-# sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev libglew-dev libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
+# sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-8-jdk freeglut3-dev libglew-dev libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
 
 # #Installing Pangolin
 # git clone https://github.com/stevenlovegrove/Pangolin.git
@@ -47,9 +55,9 @@
 # cd ..
 
 #Actually build ElasticFusion
-rm -rf Core/build/
-rm -rf GPUTest/build/
-rm -rf GUI/build/
+rm -rf ./ElasticFusion/Core/build/
+rm -rf ./ElasticFusion/GPUTest/build/
+rm -rf ./ElasticFusion/GUI/build/
 
 
 cd ./ElasticFusion/Core
