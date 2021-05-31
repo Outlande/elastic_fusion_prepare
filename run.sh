5d72d4456525d00e222542dc9339c8e1baf0f8a6
Outlande <16231215@buaa.edu.cn>
2021-05-31 19:04:33 +0800

    feed rgbd

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ sh ./ElasticFusion/build.sh
 
 # run ElasticFusion
 ./ElasticFusion/GUI/build/ElasticFusion -l ../dyson_lab.klg
+
+/home/levi/dev/meshlab/src/install/usr/bin/meshlab dyson_lab.klg.ply
